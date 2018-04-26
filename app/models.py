af4f80936c6703cdb5dab9648b113470274d09c2
Xin Liu <xhliu97@gmail.com>
2018-04-25 20:02:51 -0400

    completed user resgiteration/login/sessions

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from app import login
+from flask_login import UserMixin 
 
 # User
 class User(UserMixin, db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 # All routes for app called here
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 # Home
 @app.route('/')
+@login_required
 def index():
-	user = {'username': 'Xin'}
 	posts = [
 		{
 			'author': {'username': 'John'},
@@ -19,7 +20,7 @@ def index():
 			'body': 'The Avengers movie was so cool!'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home', posts=posts)
 
 # Login
 @app.route('/login', methods=['GET', 'POST'])
@@ -30,12 +31,18 @@ def login():
 
 	# Submit
 	form = LoginForm()
+	# Log in User
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
 		if user is None or not user.check_password(form.password.data):
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
+
+		# After User is logged in, redirect to index using next
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
 		return redirect(url_for('index'))
 
 	return render_template('login.html', title="Sign In", form=form)
@@ -45,3 +52,18 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
